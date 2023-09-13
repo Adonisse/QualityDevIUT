1fd17d2f49cfc4987cdb42cf2196646310f90b38
Adonisse <adonis.chahine@etud.u-picardie.fr>
2023-09-13 18:27:01 +0200

    Ajoutez des fichiers projet.

---
+++ td1/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+public class Media
+{
+    String titre;
+    int numRef;
+    int nbDispo;
+
+    public Media(string titre, int numRef, int nbDispo)
+    {
+        this.titre = titre;
+        this.numRef = numRef;
+        this.nbDispo = nbDispo;
+    }
+}
+
+public class Livre : Media
+{
+    String auteur;
+
+    public Livre(string titre, int numRef, int nbDispo) : base(titre, numRef, nbDispo)
+    {
+    }
+}
+
+public class DVD : Media
+{
+    int duree;
+
+    public DVD(string titre, int numRef, int nbDispo) : base(titre, numRef, nbDispo)
+    {
+    }
+}
+
+public class CD : Media
+{
+    String artiste;
+
+    public CD(string titre, int numRef, int nbDispo) : base(titre, numRef, nbDispo)
+    {
+    }
+}
